ded97b8aea18779b9efe36a3399fa17deac76653
Michael Chorak <michaelchorak96@gmail.com>
2019-01-05 18:15:50 +0100

    First commit

---
+++ src/pages/newuser/newuser.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
+/**
+ * Generated class for the NewuserPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-newuser',
+  templateUrl: 'newuser.html',
+})
+export class NewuserPage {
+  user = { id: '', first_name: '', last_name: '', email: ''};
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NewuserPage');
+  }
+
+}
